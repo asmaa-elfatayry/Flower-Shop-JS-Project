c18eb4ce731af3f2f54d78ae663df320e3236361
amira <amiramohamedmahmoud07@gmail.com>
2024-01-17 18:22:26 +0200

    push

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+
+window.addEventListener('load',function(){
+    let search_icon = this.document.querySelector(".search");
+    console.log(search_icon);
+      search_icon.addEventListener('click' , function(e){
+        let search_input = document.querySelector(".search_input");
+        search_input.classList.remove("none") ;
+        search_input.classList.add("applaySearchStyle") ;
+        
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+});
