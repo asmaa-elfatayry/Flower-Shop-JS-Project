5e775bb1ab26777fc97df973991147a71f524b3f
amira <amiramohamedmahmoud07@gmail.com>
2024-01-23 17:40:21 +0200

    push

--- JS/home.js
+++ JS/home.js
@@ -4,18 +4,15 @@ function addProduct( product)
     card = document.createElement("div");
     card.classList.add("card", "col-6", "col-md-4", "col-lg-3");
     rowDiv.appendChild(card);
-    card.innerHTML =`<div class="card-details"> 
+    card.innerHTML =`<div class="card-details">
+    <div class="icons" id="icons">
+    <a href="cart.html title="Add to cart"><i class="fa-solid fa-cart-plus"></i></a>
+    <a href ="favourites.html title="whishlist"><i class="fa-solid fa-heart"></i></a>
+    </div>
     <img src="images/flowers/${product.image}" alt="flower">
     <div class="card-body">
         <h5 class="card-title">${product.name}</h5>
-        <div class="card-rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-            <i class="fa fa-star-o"></i>
-        </div>
-        <p class="card-text">Add to Cart</p>
+        <button>Add to Cart</button>
     </div> 
     </div>`;
 }//end of addProduct
