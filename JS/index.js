30b452202bf0f46f5e3eca6af8bafa41bcdd254d
amira <amiramohamedmahmoud07@gmail.com>
2024-01-27 15:38:42 +0200

    wishlist

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-
+/*
 window.addEventListener("load", function () {
   //   const sellerId = 3; // just for test it will come from login
   function loadData() {
@@ -12,11 +12,13 @@ window.addEventListener("load", function () {
         FlowersDate = data.flowers;
         SellerData = data.sellers;
         UserData = data.users;
+        favourites = data.favourites;
         RequestSeller = data.request_seller;
         localStorage.setItem("flowersData", JSON.stringify(FlowersDate));
         localStorage.setItem("sellerData", JSON.stringify(SellerData));
         localStorage.setItem("userData", JSON.stringify(UserData));
         localStorage.setItem("requestseller", JSON.stringify(RequestSeller));
+        localStorage.setItem("favourites" ,JSON.stringify(favourites));
 
 
 
@@ -68,6 +70,65 @@ window.addEventListener("load", function () {
     //search_input.classList.remove("none");
     //search_input.classList.add("applaySearchStyle");
     //})
+  });*/
+  window.addEventListener("load", function () {
+    //   const sellerId = 3; // just for test it will come from login
+    function loadData() {
+      let FlowersDate;
+      let SellerData;
+      let UserData;
+      let RequestSeller;
+      let favourites;
+      fetch("../Data.json")
+        .then((response) => response.json())
+        .then((data) => {
+          FlowersDate = data.flowers;
+          SellerData = data.sellers;
+          UserData = data.users;
+          favourites = data.favourites;
+          RequestSeller = data.request_seller;
+          localStorage.setItem("flowersData", JSON.stringify(FlowersDate));
+          localStorage.setItem("sellerData", JSON.stringify(SellerData));
+          localStorage.setItem("userData", JSON.stringify(UserData));
+          localStorage.setItem("requestseller", JSON.stringify(RequestSeller));
+          localStorage.setItem("favourites", JSON.stringify(favourites));
+  
+          let search_icon = document.querySelector(".search");
+          console.log(search_icon);
+          search_icon.addEventListener('click', function (e) {
+            let search_input = document.querySelector(".search_input");
+            search_input.classList.remove("none");
+            search_input.classList.add("applaySearchStyle");
+          });
+        })
+        .catch((error) => console.error("Error fetching products:", error));
+    }
+  
+    if (localStorage.length == 0) {
+      loadData();
+    }
+    let CheckLogedUser = JSON.parse(sessionStorage.getItem("loggedInUser")) || [];
+    let nav = document.getElementsByClassName("user")[0];
+  
+    if (CheckLogedUser.length != 0 && CheckLogedUser.role == 'user') {
+      nav.innerHTML = `
+          <a href="../HTML Pages/user.html"><i class="fa-solid fa-user mr-5" style="color: #3498db; font-size: 20px;"></i></a>
+          <a href="#"><i class="fa fa-shopping-cart mr-5" style="color: #2ecc71; font-size: 20px;"></i></a>
+          <a href="#" class="log"><i class="fa fa-sign-out" style="color: #e74c3c; font-size: 20px;"></i></a>`;
+  
+      let logoutLink = document.querySelector(".log");
+      if (logoutLink) {
+        logoutLink.addEventListener('click', function () {
+          sessionStorage.removeItem("loggedInUser");
+          window.location.href = "../index.html";
+        });
+      }
+    } else {
+      nav.innerHTML = `
+          <a href="../HTML Pages/signup.html" class="btn btn-primary mr-2">Sign up</a>
+          <a href="../HTML Pages/login.html" class="btn btn-secondary">Login</a>`;
+    }
   });
+  
 
 
