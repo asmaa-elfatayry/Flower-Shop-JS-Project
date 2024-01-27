b2f14875273ea470eafa85f5abfc01b2f6cef4e2
ahmed fahmy <fahomahmed@gmail.com>
2024-01-27 22:48:00 +0200

    solved index.js error i don't know where it came from , it's not even my page

--- JS/index.js
+++ JS/index.js
@@ -31,43 +31,45 @@ window.addEventListener("load", function () {
         })
           .catch((error) => console.error("Error fetching products:", error));
 
-      }
- 
-  if (localStorage.length == 0) {
-      loadData();
-    }
-    let CheckLogedUser = JSON.parse(sessionStorage.getItem("loggedInUser")) || [];
-    let nav = document.getElementsByClassName("user")[0];
+      })
 
-    if (CheckLogedUser.length != 0 && CheckLogedUser.role == 'user') {
-      nav.innerHTML = `
+  }
+  if (localStorage.length == 0) {
+    loadData();
+  }
+  let CheckLogedUser = JSON.parse(sessionStorage.getItem("loggedInUser")) || [];
+  let nav = document.getElementsByClassName("user")[0];
+
+  if (CheckLogedUser.length != 0 && CheckLogedUser.role == 'user') {
+    nav.innerHTML = `
         <a href="../HTML Pages/user.html"><i class="fa-solid fa-user mr-5" style="color: #3498db; font-size: 20px;"></i></a>
         <a href="#"><i class="fa fa-shopping-cart mr-5" style="color: #2ecc71; font-size: 20px;"></i></a>
         <a href="#" class="log"><i class="fa fa-sign-out" style="color: #e74c3c; font-size: 20px;"></i></a>`;
 
-      let logoutLink = document.querySelector(".log");
-      if (logoutLink) {
-        logoutLink.addEventListener('click', function () {
-          sessionStorage.removeItem("loggedInUser");
-          window.location.href = "../index.html";
-        });
-      }
-
-    } else {
-      nav.innerHTML = `
-        <a href="../HTML Pages/signup.html" class="btn btn-primary mr-2">Sign up</a>
-        <a href="../HTML Pages/login.html" class="btn btn-secondary">Login</a>`;
+    let logoutLink = document.querySelector(".log");
+    if (logoutLink) {
+      logoutLink.addEventListener('click', function () {
+        sessionStorage.removeItem("loggedInUser");
+        window.location.href = "../index.html";
+      });
     }
 
+  } else {
+    nav.innerHTML = `
+        <a href="../HTML Pages/signup.html" class="btn btn-primary mr-2">Sign up</a>
+        <a href="../HTML Pages/login.html" class="btn btn-secondary">Login</a>`;
+  }
 
-    //amira section
-    //let search_icon = this.document.querySelector(".search");
-    //console.log(search_icon);
-    //search_icon.addEventListener('click', function (e) {
-    //let search_input = document.querySelector(".search_input");
-    //search_input.classList.remove("none");
-    //search_input.classList.add("applaySearchStyle");
-    //})
-  });
+
+  //amira section
+  //let search_icon = this.document.querySelector(".search");
+  //console.log(search_icon);
+  //search_icon.addEventListener('click', function (e) {
+  //let search_input = document.querySelector(".search_input");
+  //search_input.classList.remove("none");
+  //search_input.classList.add("applaySearchStyle");
+  //})
+
+});
 
 
